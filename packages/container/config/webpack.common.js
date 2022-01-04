438c5574dbfdf22b069f6ff29c616006fbdbd6ec
Ajit Singh <ajitkdsi@gmail.com>
2022-01-04 10:12:03 +0530

    Added auth flow

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,19 +1,16 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   module: {
     rules: [
       {
-        //all files with.m and js extsion
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            // for react jsx and for es5 convert
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-            // for runtime supprot for eg: asyn/await etc.
-            plugins: ["@babel/plugin-transform-runtime"],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
@@ -21,7 +18,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,39 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
+import Progress from './components/Progress';
+import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
-  productionPrefix = 'ma',
+  productionPrefix: 'co',
 });
 
 export default () => {
-  return (
-    <div>
-      <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
-          <Header />
-          <MarketingApp />
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/">
+                <MarketingLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+            </Switch></Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
